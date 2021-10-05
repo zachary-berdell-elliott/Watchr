ef6a651d4d13512f8fffe87628d69f56983d5b87
calvinswomley <88415921+calvinswomley@users.noreply.github.com>
2021-10-04 20:58:35 -0600

    Fetch request for Streaming Availability (#18)
    Should be approved now

--- assets/script.js
+++ assets/script.js
@@ -29,4 +29,17 @@ watchListAddBtn.click(function(){
 
 watchlistDisplayer();
 
-
+// Streaming Availability Fetch
+ fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+		"x-rapidapi-key": "514988ace5msh951fbe99f73764cp120286jsn6ee999bd6cb1"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
