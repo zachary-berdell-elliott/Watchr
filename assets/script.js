4be9c7ec8e99d20472bc3f60477389a4448bf12d
Eric Martin <eric_martin@me.com>
2021-10-04 21:00:56 -0600

    adding TMDB fetch request

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,53 @@
  
+var watchListArray = JSON.parse(localStorage.getItem("watchlist-array")) || [];
+//var watchListAddBtn = $("#watchlist-button");
+var watchList = $("#watch-list");
+
+//function for building the watchlist
+function watchlistDisplayer(){
+    //Clears the watchlist of values.
+    watchList.empty();
+
+    //Creates a new button for each saved array item
+    watchListArray.forEach(function(i){
+        var watchListBlock = $("<button>").text(i);
+        watchList.append(watchListBlock);
+        
+        //Sends the user to the watchlist item that they saved
+        watchListBlock.click(function(){
+            movieParam = watchListBlock.val();
+            getMovie(movieParam);
+        })
+    });
+}
+
+//Function to save the item to the watchlist when the user clicks the button.
+watchListAddBtn.click(function(){
+    watchListArray.push(movieName);
+    localStorage.setItem("watchlist-array", JSON.stringify(watchListArray));
+    watchlistDisplayer();
+});
+
+watchlistDisplayer();
+
+// Streaming Availability Fetch
+ fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+		"x-rapidapi-key": "514988ace5msh951fbe99f73764cp120286jsn6ee999bd6cb1"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+ // TMDB Fetch
+
+ fetch('https://api.themoviedb.org/3/movie/550?api_key=58bc4a862a66afe4f88190b44a8dd8dd')    
+
+ .then(response => response.json())
+ .then(data => console.log(data));
