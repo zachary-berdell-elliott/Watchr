6e5e2100b1715b5988612dc1f7a882cd669889c0
Zachary Elliott <zacharyberdell@gmail.com>
2021-10-04 20:31:57 -0600

    Structure for how the function for getting the data from the watchlist button would work.

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,22 @@ var watchListArray = JSON.parse(localStorage.getItem("watchlist-array")) || [];
 var watchList = $("#watch-list");
 
 function watchlistDisplayer(){
+    watchList.empty();
     watchListArray.forEach(function(i){
         var watchListBlock = $("<button>").text(i);
         watchList.append(watchListBlock);
+
+        watchListBlock.click(function(){
+            movieParam = watchListBlock.val();
+            ajaxFunc();
+        })
     });
 }
 
 watchListAddBtn.click(function(){
     watchListArray.push(movieName);
     localStorage.setItem("watchlist-array", JSON.stringify(watchListArray));
+    watchlistDisplayer();
 });
 
 watchlistDisplayer();
