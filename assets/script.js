f31adb28c5c2a59c85b43f8148304e514b1f8805
Zachary Elliott <zacharyberdell@gmail.com>
2021-10-04 19:39:44 -0600

    Function to fetch items from local storage and append them to the watchlist built.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
- 
+var watchListArray = JSON.parse(localStorage.getItem("watchlist-array")) || [];
+//var watchListAddBtn = $("#watchlist-button");
+var watchList = $("#watch-list");
+
+function watchlistDisplayer(){
+    watchListArray.forEach(function(i){
+        var watchListBlock = $("<h4>").text(i);
+        watchList.append(watchListBlock);
+    })
+}
+
+watchlistDisplayer();
+
+
